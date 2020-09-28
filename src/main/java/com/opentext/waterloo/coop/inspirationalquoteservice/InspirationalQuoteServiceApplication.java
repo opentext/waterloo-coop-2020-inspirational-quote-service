2c80a6641af7a85c65510f3c9766b9a5a6930310
Jason Ibrahim <jasonaibrahim@gmail.com>
2020-09-28 11:03:57 -0700

    initial commit

---
+++ src/main/java/com/opentext/waterloo/coop/inspirationalquoteservice/InspirationalQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.opentext.waterloo.coop.inspirationalquoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspirationalQuoteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InspirationalQuoteServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/opentext/waterloo/coop/inspirationalquoteservice/InspirationalQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opentext.waterloo.coop.inspirationalquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspirationalQuoteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
