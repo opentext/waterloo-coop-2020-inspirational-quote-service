34d4e1eaf647f9c9d097f5057a95c426bf00c102
Jindong <jindongche@Jindongs-MacBook-Pro.local>
2020-09-29 16:54:40 -0400

    added caching

--- src/main/java/com/opentext/waterloo/coop/inspirationalquoteservice/Controller.java
+++ src/main/java/com/opentext/waterloo/coop/inspirationalquoteservice/Controller.java
@@ -1,6 +1,7 @@
 package com.opentext.waterloo.coop.inspirationalquoteservice;
 
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,45 +12,23 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import org.springframework.cache.annotation.Cacheable;
 
 @RestController
 public class Controller {
 
+    @Autowired
+    private final fetchAPI response;
+
+    public Controller(fetchAPI result) {
+        this.response = result;
+    }
+
     @GetMapping("/quote")
     public Quote quote(@RequestParam(value = "name", defaultValue = "World") String name) throws Exception {
         //fetch live response
-        String builder = null;
-        try {
-            URL url = new URL("https://quotes.rest/qod?category=inspire");
-            HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
-            urlc.setRequestMethod("GET");
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
-            StringBuilder sb = new StringBuilder();
-            String line;
-
-            while ((line=br.readLine())!=null) {
-                sb.append(line).append('\n');
-            }
-            br.close();
-            builder = sb.toString();
-        } catch (Exception e){
-            System.out.println("Error occured " + e.getMessage());
-        }
-
-//        //read local json file
-//        Resource resource = new ClassPathResource("plain.json");
-//        File file = resource.getFile();
-//
-//        BufferedReader fileReader = new BufferedReader(new FileReader(file));
-//        StringBuilder builder = new StringBuilder();
-//        String r;
-//        while ((r=fileReader.readLine())!=null) {
-//            builder.append(r);
-//        }
-//        fileReader.close();
-
-        JSONObject json = new JSONObject(builder.toString());
+        JSONObject json = response.getAPI();
         JSONObject quote = new JSONObject(json.getJSONObject("contents").getJSONArray("quotes").getString(0));
 
         String quoteOfTheDay = quote.get("quote").toString();
@@ -60,7 +39,7 @@ public class Controller {
         String permalink = json.getJSONObject("copyright").get("url").toString();
 
         //missing numberOfCalls
-        return new Quote(quoteOfTheDay, timestamp, -1, author, language, image, permalink);
+        return new Quote(quoteOfTheDay, timestamp, -1, author, language, image, "permalink");
 //        Quote(String quoteOfTheDay, String timestamp, int numberOfCalls, String author, String language, String image, String permalink)
     }
 }
--- src/main/java/com/opentext/waterloo/coop/inspirationalquoteservice/InspirationalQuoteServiceApplication.java
+++ src/main/java/com/opentext/waterloo/coop/inspirationalquoteservice/InspirationalQuoteServiceApplication.java
@@ -9,27 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class InspirationalQuoteServiceApplication {
-
-    private static final Logger log = LoggerFactory.getLogger(InspirationalQuoteServiceApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(InspirationalQuoteServiceApplication.class, args);
     }
 
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
-    }
-
-    @Bean
-    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-        return args -> {
-            Quote quote = restTemplate.getForObject(
-                    "https://quotes.rest/qod?category=inspire", Quote.class);
-            log.info(quote.toString());
-        };
-    }
 }
