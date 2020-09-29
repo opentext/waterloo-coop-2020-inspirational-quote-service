3c77bff96b6c7fd0b9bdf97f0f79b0b9f83fe60b
Tenyage <tenyage@gmail.com>
2020-09-29 11:16:02 -0400

    Updated api fetching logic

--- src/main/java/com/opentext/waterloo/coop/inspirationalquoteservice/Controller.java
+++ src/main/java/com/opentext/waterloo/coop/inspirationalquoteservice/Controller.java
@@ -1,67 +1,66 @@
 package com.opentext.waterloo.coop.inspirationalquoteservice;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.concurrent.atomic.AtomicLong;
-import java.io.File;
 
 import org.json.JSONObject;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.core.io.ClassPathResource;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 public class Controller {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-
     @GetMapping("/quote")
     public Quote quote(@RequestParam(value = "name", defaultValue = "World") String name) throws Exception {
-        URL url = new URL("https://quotes.rest/qod?category=funny");
-        StringBuilder buffer = new StringBuilder();
+        //fetch live response
+        String builder = null;
         try {
+            URL url = new URL("https://quotes.rest/qod?category=inspire");
             HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
             urlc.setRequestMethod("GET");
-            // set the content type
-            urlc.setRequestProperty("Content-Type", "application/json");
-            urlc.setRequestProperty("X-TheySaidSo-Api-Secret", "YOUR API KEY HERE");
-            //System.out.println("Connect to: " + url.toString());
-            urlc.setAllowUserInteraction(false);
-            urlc.connect();
 
             BufferedReader br = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
-            String l = null;
+            StringBuilder sb = new StringBuilder();
+            String line;
 
-            //while ((l=br.readLine())!=null) {
-            //    buffer.append(l);
-            //}
+            while ((line=br.readLine())!=null) {
+                sb.append(line).append('\n');
+            }
             br.close();
+            builder = sb.toString();
         } catch (Exception e){
-            System.out.println("Error occured ???");
-            System.out.println(e.toString());
+            System.out.println("Error occured " + e.getMessage());
         }
 
-        Resource resource = new ClassPathResource("plain.json");
-        File file = resource.getFile();
+//        //read local json file
+//        Resource resource = new ClassPathResource("plain.json");
+//        File file = resource.getFile();
+//
+//        BufferedReader fileReader = new BufferedReader(new FileReader(file));
+//        StringBuilder builder = new StringBuilder();
+//        String r;
+//        while ((r=fileReader.readLine())!=null) {
+//            builder.append(r);
+//        }
+//        fileReader.close();
 
-        BufferedReader fileReader = new BufferedReader(new FileReader(file));
-        String r;
-        while ((r=fileReader.readLine())!=null) {
-            buffer.append(r);
-        }
-
-        JSONObject json = new JSONObject(buffer.toString());
+        JSONObject json = new JSONObject(builder.toString());
         JSONObject quote = new JSONObject(json.getJSONObject("contents").getJSONArray("quotes").getString(0));
 
+        String quoteOfTheDay = quote.get("quote").toString();
+        String timestamp = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now());
+        String author = quote.get("author").toString();
+        String language = quote.get("language").toString();
+        String image = quote.get("background").toString();
+        String permalink = json.getJSONObject("copyright").get("url").toString();
 
-
-        return new Quote(-1, quote.getString("quote"));
+        //missing numberOfCalls
+        return new Quote(quoteOfTheDay, timestamp, -1, author, language, image, permalink);
+//        Quote(String quoteOfTheDay, String timestamp, int numberOfCalls, String author, String language, String image, String permalink)
     }
 }
