732fc84f448b086a0a93bff6bc66872bf5a27151
Shashank Anand Kannan <sanandkannan@opentext.com>
2020-09-30 15:45:29 -0400

    Improve tests

--- src/test/java/com/opentext/waterloo/coop/inspirationalquoteservice/LocalRepoTest.java
+++ src/test/java/com/opentext/waterloo/coop/inspirationalquoteservice/LocalRepoTest.java
@@ -1,4 +1,23 @@
 package com.opentext.waterloo.coop.inspirationalquoteservice;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class LocalRepoTest {
+
+    @Autowired
+    private LocalQuoteRepository localQuoteRepository;
+
+    @Test
+    public void contextLoads() {
+        assertThat(localQuoteRepository).isNotNull();
+    }
+
 }
