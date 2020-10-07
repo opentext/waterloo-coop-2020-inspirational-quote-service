6a02e9178c6f67c6a4b00cbcaaf691d15378c059
souyang-opentext <souyang@opentext.com>
2020-10-07 16:19:37 -0400

    RemoteQuoteRepository connection timeout

--- src/main/java/com/opentext/waterloo/coop/inspirationalquoteservice/RemoteQuoteRepository.java
+++ src/main/java/com/opentext/waterloo/coop/inspirationalquoteservice/RemoteQuoteRepository.java
@@ -6,11 +6,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -30,6 +31,7 @@ public class RemoteQuoteRepository implements QuoteRepository{
             URL url = new URL("https://quotes.rest/qod?category=inspire");
             HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
             urlc.setRequestMethod("GET");
+            urlc.setConnectTimeout(2500);
 
             BufferedReader br = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
             StringBuilder sb = new StringBuilder();
@@ -40,11 +42,24 @@ public class RemoteQuoteRepository implements QuoteRepository{
             }
             br.close();
             builder = sb.toString();
-        } catch (Exception e){
-            System.out.println("Error occurred " + e.getMessage());
+        } catch (java.net.SocketTimeoutException e) {
+            log.error("Connection timeout! " + e.getMessage());
+        } catch (java.io.IOException e) {
+            log.error("IOException occurred! " + e.getMessage());
         }
-
+        if (builder != null){
+            writeUsingFileWriter(builder);
+        }
         return new JSONObject(builder);
+    }
+
+    private void writeUsingFileWriter(String data) throws FileNotFoundException {
+        PrintWriter writer =
+                new PrintWriter(
+                        new File(this.getClass().getResource("plain.json").getPath()));
+        writer.write(data);
+        writer.flush();
+        writer.close();
     }
 
     //scheduled flush at midnight
