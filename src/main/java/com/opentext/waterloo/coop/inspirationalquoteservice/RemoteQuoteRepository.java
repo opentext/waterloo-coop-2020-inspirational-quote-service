d4da572a8b3332c498401110365807981894be36
souyang-opentext <souyang@opentext.com>
2020-10-08 10:40:25 -0400

    RemoteQuoteRepository overwrite plain.json

--- src/main/java/com/opentext/waterloo/coop/inspirationalquoteservice/RemoteQuoteRepository.java
+++ src/main/java/com/opentext/waterloo/coop/inspirationalquoteservice/RemoteQuoteRepository.java
@@ -3,18 +3,24 @@ package com.opentext.waterloo.coop.inspirationalquoteservice;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
 
 @Component
 public class RemoteQuoteRepository implements QuoteRepository{
+
+    @Autowired
+    Environment env;
 
     private static final Logger log = LoggerFactory.getLogger(InspirationalQuoteServiceApplication.class);
 
@@ -29,7 +35,7 @@ public class RemoteQuoteRepository implements QuoteRepository{
             URL url = new URL("https://quotes.rest/qod?category=inspire");
             HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
             urlc.setRequestMethod("GET");
-            urlc.setConnectTimeout(2500);
+            urlc.setConnectTimeout(3000);
 
             BufferedReader br = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
             StringBuilder sb = new StringBuilder();
@@ -45,19 +51,25 @@ public class RemoteQuoteRepository implements QuoteRepository{
         } catch (java.io.IOException e) {
             log.error("IOException occurred! " + e.getMessage());
         }
-        if (builder != null){
-            writeUsingFileWriter(builder);
+        // only enable file overwriting file in active dev environment
+        if(Arrays.stream(env.getActiveProfiles()).anyMatch(
+                env -> (env.equalsIgnoreCase("dev"))) && builder != null) {
+            writeUsingBufferedWriter(builder);
         }
         return new JSONObject(builder);
     }
 
-    private void writeUsingFileWriter(String data) throws FileNotFoundException {
-        PrintWriter writer =
-                new PrintWriter(
-                        new File(this.getClass().getResource("plain.json").getPath()));
-        writer.write(data);
-        writer.flush();
-        writer.close();
+    private void writeUsingBufferedWriter(String data) {
+        File file = null;
+        try {
+            file = new File("src/main/resources/plain.json");
+            BufferedWriter br = new BufferedWriter(new FileWriter(file));
+            br.write(data);
+            br.flush();
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     //scheduled flush at midnight
